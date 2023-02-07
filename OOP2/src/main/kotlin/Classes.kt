d7d214010ab1e9fb0a16896a71be8d02b0b68366
CalibreXx <48781590+CalibreXx@users.noreply.github.com>
2023-02-07 22:26:51 +0800

    Update

--- OOP2/src/main/kotlin/Classes.kt
+++ OOP2/src/main/kotlin/Classes.kt
@@ -1,2 +1,46 @@
-class Classes {
+
+class ListView(val items: Array<String>) {
+    inner class ListViewItems() {
+        fun displayItem(position: Int) {
+            //inner classs have acess to outer class
+            println(items[position])
+        }
+    }
+}
+
+class Car(val name: String, val color: String, val engines: Int) {
+    fun move(){
+        println("$name is moving")
+    }
+    fun stop(){
+        println("$name is stopped")
+    }
+}
+
+class Plane (val name: String, val color: String, val engines: Int){ //same as the above!!!!
+    fun move(){
+        println("$name is moving")
+    }
+    fun stop(){
+        println("$name is stopped")
+    }
+}
+
+open class Vehicles(val name: String, val color: String){
+    open fun move(){
+        println("$name is moving")
+    }
+    open fun stop(){
+        println("$name is stopped")
+    }
+}
+
+class Car1(name: String, color: String, val engines: Int) : Vehicles (name, color){
+    override fun move() {
+        flying() // new function
+        super.move() // old function
+    }
+    fun flying(){
+        println("$name is driving")
+    }
 }
--- OOP2/src/main/kotlin/Main.kt
+++ OOP2/src/main/kotlin/Main.kt
@@ -1,37 +1,62 @@
+import java.lang.Exception
+import javax.naming.InvalidNameException
+
 /*
     -inner class
-    -inherited class from another class
+    -inherited class from another class add open
+        open class MainClass()
+        class inheritor():MainClass
+    -overwrite inherited class function by adding open
+        code/generate/overwrite methods
+        overwrite fun
+    -Sealed classses
  */
 
 
 fun main() {
     val listview = ListView(arrayOf("name1", "name2", "name3"))
     listview.ListViewItems().displayItem(1)
+
+    val cars = Car1("mine", "white", 35)
+    cars.move() //functions have been inherited
+    cars.stop()
+
+    val success = Result.Sucesss("suxxxess")
+    val error = Result.Error.NonrecoverableError(exception= InvalidNameException, message = "invalid name")
+    getData(error)
+
 }
 
-class ListView(val items: Array<String>) {
-    inner class ListViewItems() {
-        fun displayItem(position: Int) {
-            //inner classs have acess to outer class
-            println(items[position])
-        }
+fun getData(ab: Result) {
+    when (ab) {
+        is Result.Sucesss -> ab.showMessageSucess()
+        is Result.Progress -> ab.showMessageProgress()
+        is Result.Error.NonrecoverableError -> ab.showMessageError()
+        is Result.Error.RecoverableError -> ab.showMessageError()
     }
 }
 
-class Car(val name: String, val color: String, val engines: Int) {
-    fun move(){
-        println("$name is moving")
+sealed class Result(val message: String) {
+    fun showMessageSucess() {
+        println("sucess Result: $message")
     }
-    fun stop(){
-        println("$name is stopped")
+
+    fun showMessageError() {
+        println("error Result: $message")
     }
+
+    fun showMessageProgress() {
+        println("progress Result: $message")
+    }
+
+    class Sucesss(message: String) : Result(message)
+    sealed class Error(message: String) : Result(message){
+        class RecoverableError(exception: Exception, message: String): Error(message)
+        class NonrecoverableError(exception: Exception, message: String):Error(message)
+    }
+    class Progress(message: String) : Result(message)
+
 }
 
-class Plane (val name: String, val color: String, val engines: Int){ //same as the above!!!!
-    fun move(){
-        println("$name is moving")
-    }
-    fun stop(){
-        println("$name is stopped")
-    }
-}
+
+
